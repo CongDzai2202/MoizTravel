e676158fab3c2670692379b88f12cf0f64c74ec3
khonglotim123 <xagaph13499@fpt.edu.vn>
2022-10-24 13:44:11 +0700

    CRUD API TourDetail

---
+++ MoizTravel/MoizTravel.WebAPI/Controllers/TourDetailController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoizTravel.Model.ViewModel.Tour;
+using MoizTravel.WebAPI.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoizTravel.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TourDetailController : ControllerBase
+    {
+        private ITourDetailRepository _tourDetail;
+        public TourDetailController(ITourDetailRepository tourDetail)
+        {
+            _tourDetail = tourDetail;
+        }
+        [HttpGet]
+        public IActionResult GetList()
+        {
+            List<TourDetailViewModel> lst = _tourDetail.GetList();
+            return Ok(lst);
+        }
+        [HttpPost]
+        public IActionResult Create(TourDetailViewModel tourDetailView)
+        {
+            var a = _tourDetail.Create(tourDetailView);
+            return CreatedAtAction(nameof(Create), a);
+        }
+        [HttpPut]
+        public IActionResult Update(TourDetailViewModel tourDetailView)
+        {
+            _tourDetail.Update(tourDetailView);
+            return Ok();
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _tourDetail.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ MoizTravel/MoizTravel.WebAPI/Repositories/TourDetailRepository.cs
@@ -0,0 +1,71 @@
+﻿using MoizTravel.Model.ViewModel.Tour;
+using MoizTravel.WebAPI.DbContext;
+using MoizTravel.WebAPI.Entities;
+using MoizTravel.WebAPI.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoizTravel.WebAPI.Repositories
+{
+    public class TourDetailRepository : ITourDetailRepository
+    {
+        private readonly MoizTravelDbContext _context;
+        public TourDetailRepository(MoizTravelDbContext context)
+        {
+            _context = context;
+        }
+        public bool Create(TourDetailViewModel tourDetailView)
+        {
+            if (tourDetailView == null) return false;
+            _context.tourDetails.Add(new TourDetail()
+            {
+                TourId = tourDetailView.TourId,
+                TourGuiderId = tourDetailView.TourGuiderId,
+                TourDetailName = tourDetailView.TourDetailName,
+                TotalAmount = tourDetailView.TotalAmount,
+                Status = 0
+            });
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            if (id == null) return false;
+            TourDetail tourDetail = _context.tourDetails.FirstOrDefault(c => c.TourDetailId == id);
+            tourDetail.Status = 1;
+            _context.tourDetails.Update(tourDetail);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public List<TourDetailViewModel> GetList()
+        {
+            return _context.tourDetails.Select(c => new TourDetailViewModel()
+            {
+                TourDetailId=c.TourDetailId,
+                TourId=c.TourId,
+                TourGuiderId=c.TourGuiderId,
+                TourDetailName=c.TourDetailName,
+                TotalAmount=c.TotalAmount,
+                Status=c.Status
+            }).ToList();
+        }
+
+        public bool Update(TourDetailViewModel tourDetailView)
+        {
+            if (tourDetailView == null) return false;
+            _context.tourDetails.Update(new TourDetail()
+            {
+                TourId = tourDetailView.TourId,
+                TourGuiderId = tourDetailView.TourGuiderId,
+                TourDetailName = tourDetailView.TourDetailName,
+                TotalAmount = tourDetailView.TotalAmount
+            });
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
